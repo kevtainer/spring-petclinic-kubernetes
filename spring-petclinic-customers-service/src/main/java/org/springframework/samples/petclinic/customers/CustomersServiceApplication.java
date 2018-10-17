1b0c8ad44376aedbd68e6ff8e11423cf43ee5566
Kevin Crawley <kevin@nashdocker.io>
2018-10-16 21:00:21 -0500

    added jaeger tracer and client config

--- spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
+++ spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
@@ -15,6 +15,7 @@
  */
 package org.springframework.samples.petclinic.customers;
 
+import io.jaegertracing.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -26,4 +27,10 @@ public class CustomersServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomersServiceApplication.class, args);
 	}
+
+    @Bean
+    @Profile("jaegertracing")
+    public io.opentracing.Tracer getTracer() {
+        return Configuration.fromEnv().getTracer();
+    }
 }
--- spring-petclinic-owners-service/src/main/java/org.springframework.samples.petclinic.owners/OwnersApplication.java
+++ spring-petclinic-owners-service/src/main/java/org.springframework.samples.petclinic.owners/OwnersApplication.java
@@ -15,6 +15,7 @@
  */
 package org.springframework.samples.petclinic.owners;
 
+import io.jaegertracing.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -27,6 +28,12 @@ public class OwnersApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OwnersApplication.class, args);
+    }
+
+    @Bean
+    @Profile("jaegertracing")
+    public io.opentracing.Tracer getTracer() {
+        return Configuration.fromEnv().getTracer();
     }
 
     @Bean
