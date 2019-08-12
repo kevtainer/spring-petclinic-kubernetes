a9c527eb963272bad18aee964a32447a1ae849dd
Kevin Crawley <kcrawley@franklinamerican.com>
2019-08-12 13:49:04 -0500

    fix dependency in visits; added o11y stuff to notifications

--- spring-petclinic-notifications-service/src/main/java/org/springframework/samples/petclinic/notifications/NotificationsServiceApplication.java
+++ spring-petclinic-notifications-service/src/main/java/org/springframework/samples/petclinic/notifications/NotificationsServiceApplication.java
@@ -4,12 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
+
+import io.jaegertracing.Configuration;
 
 @SpringBootApplication
 public class NotificationsServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationsServiceApplication.class, args);
+    }
+
+    @Bean
+    @Profile("jaegertracing")
+    public io.opentracing.Tracer getTracer() {
+        return Configuration.fromEnv().getTracer();
     }
 
     @Bean
